615d259e9f407cd2a1d95db16ac9c126196eb6bc
Danilo Mendes <danilodev.br@gmail.com>
2025-03-17 00:49:10 -0300

    Added main Spring Boot application

---
+++ src/main/java/com/DaniloMendes/ct_master/CtmasterApplication.java
@@ -0,0 +1,14 @@
+package com.DaniloMendes.ct_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CtmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtmasterApplication.class, args);
+	}
+
+}
